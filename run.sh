a36ab59c93787fc8c7a13611967e3efefc98603e
Varik96 <riku.vaurio@outlook.com>
2018-05-15 20:49:13 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ sudo apt-get install -y git salt-minion
 
 git clone https://github.com/Varik96/Samba-installation.git
 
+sudo mkdir -p /srv/samba/share/
+
+sudo chown -R nobody:nogroup /srv/samba/share/
+
 cd Samba-installation/
 
 bash highstate.sh
